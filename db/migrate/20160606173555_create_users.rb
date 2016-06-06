a2a466c87324803aa23de9df07c7c39de5b017f8
Dayana Mick <dayanamick@gmail.com>
2016-06-06 19:38:32 +0200

    correct scaffold command adding User

---
+++ db/migrate/20160606173555_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :city
+      t.string :country
+      t.string :email
+      t.integer :age
+
+      t.timestamps null: false
+    end
+  end
+end
