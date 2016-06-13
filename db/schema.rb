f4478a7f4405c55f0acafd8f04ef1eea02417930
Dayana Mick <dayanamick@gmail.com>
2016-06-13 19:58:38 +0200

    Postgres setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160606173555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "city"
