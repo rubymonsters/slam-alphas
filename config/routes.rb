c8dec231d4ec89bfa85d1cfb70b367f36a319c57
Dayana Mick <dayanamick@gmail.com>
2016-06-13 21:19:55 +0200

    add about page, homepage and 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+
+  get "/pages/:id" => "pages#show"
+  root "pages#show", id: "home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
