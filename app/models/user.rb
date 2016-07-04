d3849f07610906d5c0751394ea3179bd98a08b59
Thuy Le <inthuytion@gmail.com>
2016-07-04 21:49:23 +0000

    Add uniqueness validation for email on create & update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 include ActiveModel::Validations
 	validates :name, :city, :email, presence: true
+	validates :email, uniqueness: { case_sensitive: false }, on: :create
+	validates :email, uniqueness: { case_sensitive: false }, on: :update
 end
