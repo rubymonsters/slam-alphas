23c93cf688000e6e54ae90424721483c7f611ef9
Thuy Le <inthuytion@gmail.com>
2016-07-04 22:10:31 +0000

    Add email validation on validity

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ include ActiveModel::Validations
 	validates :name, :city, :email, presence: true
 	validates :email, uniqueness: { case_sensitive: false }, on: :create
 	validates :email, uniqueness: { case_sensitive: false }, on: :update
+
+	# customized validity check in app/validators/email_validator.rb
+	validates :email, email: true
 end
