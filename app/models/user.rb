865f4973b8912a6e559dc8188a3ecec30da81e9a
Thuy Le <inthuytion@gmail.com>
2016-07-05 06:14:31 +0000

    Refactor uniqueness validation for emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
 include ActiveModel::Validations
 	validates :name, :city, :email, presence: true
 	validates :name, length: { minimum: 2 }
-	validates :email, uniqueness: { case_sensitive: false }, on: :create
-	validates :email, uniqueness: { case_sensitive: false }, on: :update
+	validates :email, uniqueness: { case_sensitive: false }, on: [:create, :update]
 
 	# customized validity check in app/validators/email_validator.rb
 	validates :email, email: true
+
+  # customized validity check in app/validators/website_validator.rb
+	validates :website, website: true
 end
