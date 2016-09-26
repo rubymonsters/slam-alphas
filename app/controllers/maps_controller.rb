acb1c878de2b64b4be7a502993cd679521e843ce
eine-sandra <info@sandra-strack.de>
2016-09-26 21:24:40 +0200

    add support for three different maps

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,16 @@
 class MapsController < ApplicationController
-  def show
-    @users = User.where(country: map_params[:country])
 
+CENTERS = {"de"=> [50.931, 11.272], "at"=> [47.61, 13.78], "ch"=> [46.87, 8.24]}
+ZOOMS = {"de"=> 6, "at"=> 7, "ch"=> 7}
+
+  def show
+  	if ["de", "at", "ch"].include?(map_params[:country])
+      @users = User.where(country: map_params[:country])
+      @center = CENTERS[map_params[:country]]
+      @zoom = ZOOMS[map_params[:country]]
+    else 
+    	render plain: "404 Not Found", status: 404
+    end	
   end
 
   private
