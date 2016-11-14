5c08680b063142eba4a4870cb63b04ed486acecd
Thuy Le <inthuytion@gmail.com>
2016-11-14 20:07:28 +0000

    Remove execption for new method in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "users", except: [:new] do
+  resources :users, controller: "users" do
     member do
       get :change_password
       patch :update_password
