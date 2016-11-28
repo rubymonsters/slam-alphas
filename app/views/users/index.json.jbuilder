0e5a6eb9ff428f243059d8b5c6805d2708e6e1b7
eine-sandra <info@sandra-strack.de>
2016-11-28 21:26:12 +0100

    Did more translation, changed age to year of birth

--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@users) do |user|
-  json.extract! user, :id, :name, :city, :country, :email, :age
+  json.extract! user, :id, :name, :city, :country, :email, :year_of_birth
   json.url user_url(user, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161024172618) do
+ActiveRecord::Schema.define(version: 20161128200204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161024172618) do
     t.string   "city"
     t.string   "country"
     t.string   "email"
-    t.integer  "age"
+    t.integer  "year_of_birth"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.string   "website"
