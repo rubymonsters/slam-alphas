852a2c7a475db2a84f0fb561581b6d7fa28436be
lislis <mail@lislis.de>
2016-12-05 21:26:32 +0100

    start naive approach to alphabetical list

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,14 +4,15 @@ CENTERS = {"de"=> [50.931, 11.272], "at"=> [47.61, 13.78], "ch"=> [46.87, 8.24]}
 ZOOMS = {"de"=> 6, "at"=> 7, "ch"=> 7}
 
   def show
-  	if ["de", "at", "ch"].include?(map_params[:country])
+    if ["de", "at", "ch"].include?(map_params[:country])
       @users = User.where(country: map_params[:country])
       @center = CENTERS[map_params[:country]]
       @zoom = ZOOMS[map_params[:country]]
       @cities = User.all.pluck(:city).uniq
     else
-    	render plain: "404 Not Found", status: 404
+      render plain: "404 Not Found", status: 404
     end
+    @alphas = User.all.sort_by {|x| x.name }
   end
 
   private
