690ce2a6159d4cb179c0f51035b20b9ecc3adf7c
Thuy Le <inthuytion@gmail.com>
2016-12-19 20:32:36 +0100

    Add admin and public columns

---
+++ db/migrate/20161219190441_add_public_and_admin_to_users.rb
@@ -0,0 +1,6 @@
+class AddPublicAndAdminToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :public, :boolean, default: false
+    add_column :users, :admin, :boolean, default: false
+  end
+end
