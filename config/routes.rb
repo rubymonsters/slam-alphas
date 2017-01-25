d9db3ba52b20f91bc925addc342137ce2d29e6ee
lislis <mail@lislis.de>
2017-01-25 18:26:17 +0100

    fix rebase errors

--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root to: redirect("karte")
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
