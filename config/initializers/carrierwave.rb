25971da1873ae6b8595c880912c20f8e8ac07625
Thuy Le <inthuytion@gmail.com>
2017-02-13 23:58:51 +0000

    Add example configs for fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,15 @@ CarrierWave.configure do |config|
   config.ignore_integrity_errors = false
   config.ignore_processing_errors = false
   config.ignore_download_errors = false
+
+  # Configure fog for AWS s3
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => 'xxx',       # required
+    :aws_secret_access_key  => 'yyy',       # required
+    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'name_of_your_aws_bucket' # required
+  # see https://github.com/jnicklas/carrierwave#using-amazon-s3
+  # for more optional configuration
 end
