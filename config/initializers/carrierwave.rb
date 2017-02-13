31044ee23a3659d2662f0614d8a420e1662aa339
Thuy Le <inthuytion@gmail.com>
2017-02-13 23:49:56 +0000

    Switch from :file to :fog for storage in initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.permissions = 0666
   config.directory_permissions = 0777
-  config.storage = :file
+  config.storage = :fog
   config.ignore_integrity_errors = false
   config.ignore_processing_errors = false
   config.ignore_download_errors = false
