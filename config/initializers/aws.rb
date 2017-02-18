efcc6b6a39d15031702f223f4974c61feeecdc6d
Thuy Le <inthuytion@gmail.com>
2017-02-18 13:49:35 +0000

    Install and config AWS sdk to interact between s3 and heroku.
    This setting will make sure to load the latest AWS credentials and configs to interact between S3 and heroku.
    For further infos please see: https://devcenter.heroku.com/articles/direct-to-s3-image-uploads-in-rails#s3-sdk

---
+++ config/initializers/aws.rb
@@ -0,0 +1,7 @@
+# Ensures to use up-to-date AWS s3 credentials for API calls
+Aws.config.update({
+  region: 'eu-west-1',
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+})
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
