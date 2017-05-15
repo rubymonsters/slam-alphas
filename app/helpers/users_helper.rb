ed005b43ae720571ab219971364026c50a99b607
lislis <lislis@users.noreply.github.com>
2017-05-16 00:38:05 +0200

    User UX tweaks for admins and non-JS, closes #73 (#95)
    * add JS free way to access slammers
    
    without JS enabled the map is useless.
    Though I assume that most users will have JS enabled
    offering a database of information that requires JS to
    access information feels wrong.
    
    change tile set to higher contrast, black and white
    
    revamp user#index to make it more admin friendly
    
    cleanup templates, styles and differentiate in admin bar
    
    also offer non-JS link on user#show
    
    tweak map
    
    * fix map control hover

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
       return "empfohlen von #{user.recommended_by_alpha}"
     end
   end
+
+  def admin_user_public?(current_user, user)
+    if current_user && current_user.admin? && user.public
+      true
+    else
+      false
+    end
+  end
 end
