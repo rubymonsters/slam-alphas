c4987973ef3508e59339e9cb13c0ffa2c960353a
lislis <lislis@users.noreply.github.com>
2017-09-22 12:36:26 +0200

    change mailer url to https (#186)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'karte.slamalphas.org' }
+  config.action_mailer.default_url_options = {
+    host: 'karte.slamalphas.org',
+    protocol: 'https'
+  }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
