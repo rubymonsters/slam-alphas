e848fb4a4dd09781bfb5ff20c0cea6836b6e06d1
lislis <mail@lislis.de>
2017-11-10 14:01:17 +0100

    try caching geocoder results in redis

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'carrierwave', '~> 1.0'
 gem 'carrierwave_direct'
 gem 'fog', '~> 1.38' # cloud services library for AWS s3
 gem 'exception_notification'
+gem 'redis'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,2 +1,14 @@
-# Set API response timeout in seconds here (useful for seeding data into the database)
-Geocoder::Configuration.timeout = 60
+require 'redis'
+
+REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+
+Geocoder.configure(
+  lookup: :google,
+  cache: REDIS,
+  always_raise: [
+    Geocoder::OverQueryLimitError,
+    Geocoder::RequestDenied,
+    Geocoder::InvalidRequest,
+    Geocoder::InvalidApiKey
+  ]
+)
