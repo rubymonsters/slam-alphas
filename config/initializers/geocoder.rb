38397d34d0e432bb9274ad46720935c62081abd9
lislis <mail@lislis.de>
2017-11-20 22:37:42 +0100

    only set up geocoder cache in production

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,16 @@
 require 'redis'
 
-REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+if Rails.env.production?
+  REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
 
-Geocoder.configure(
-  lookup: :google,
-  cache: REDIS,
-  always_raise: [
-    Geocoder::OverQueryLimitError,
-    Geocoder::RequestDenied,
-    Geocoder::InvalidRequest,
-    Geocoder::InvalidApiKey
-  ]
-)
+  Geocoder.configure(
+    lookup: :google,
+    cache: REDIS,
+    always_raise: [
+      Geocoder::OverQueryLimitError,
+      Geocoder::RequestDenied,
+      Geocoder::InvalidRequest,
+      Geocoder::InvalidApiKey
+    ]
+  )
+end
