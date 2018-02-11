6dc4bc0734a532b425a8b65c737e48e549eea0c5
lislis <mail@lislis.de>
2018-02-11 15:04:56 +0100

    send new user email to office@

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < Clearance::UsersController
         sign_in @user
 
         UserMailer.welcome(@user).deliver_now
-        User.admin.each { |u| UserMailer.new_user(u, @user).deliver_now }
+        UserMailer.new_user("office@slamalphas.org", @user).deliver_now
 
         format.html { redirect_to @user, notice: 'Danke! Du bekommst eine E-Mail sobald dein Profil öffentlich zugänglich ist.' }
         format.json { render :show, status: :created, location: @user }
