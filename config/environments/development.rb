d8939a4232c277341ef419096b704ebf41ca7bd6
lislis <mail@lislis.de>
2018-04-18 22:54:29 +0200

    start rails upgrade

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -50,5 +53,4 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
-
 end
