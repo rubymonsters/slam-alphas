07d8ac9e3296528be547786432d8f3354f8dbc2c
lislis <mail@lislis.de>
2019-08-13 21:05:49 +0200

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,46 +15,45 @@ ActiveRecord::Schema.define(version: 20180408175736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.string   "url"
-    t.string   "city"
-    t.integer  "user_id"
-    t.integer  "relationship"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "events", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "city"
+    t.integer "user_id"
+    t.integer "relationship"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "city"
-    t.string   "country"
-    t.string   "email"
-    t.integer  "year_of_birth"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "website"
-    t.string   "facebook_link"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "encrypted_password",  limit: 128
-    t.string   "confirmation_token",  limit: 128
-    t.string   "remember_token",      limit: 128
-    t.boolean  "public",                          default: false
-    t.boolean  "admin",                           default: false
-    t.boolean  "alpha",                           default: false, null: false
-    t.integer  "recommended_by",                  default: 0
-    t.text     "video_link"
-    t.text     "is_available_on",                 default: [],                 array: true
-    t.string   "will_travel"
-    t.text     "travels_via",                     default: [],                 array: true
-    t.text     "train_bonus_card",                default: [],                 array: true
-    t.string   "avatar"
-    t.text     "custom_availability",             default: "",    null: false
-    t.text     "bookable_as",                     default: [],                 array: true
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "country"
+    t.string "email"
+    t.integer "year_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "website"
+    t.string "facebook_link"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.boolean "public", default: false
+    t.boolean "admin", default: false
+    t.string "avatar"
+    t.text "video_link"
+    t.boolean "alpha", default: false, null: false
+    t.text "is_available_on", default: [], array: true
+    t.integer "recommended_by", default: 0
+    t.string "will_travel"
+    t.text "travels_via", default: [], array: true
+    t.text "train_bonus_card", default: [], array: true
+    t.text "custom_availability", default: "", null: false
+    t.text "bookable_as", default: [], array: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
