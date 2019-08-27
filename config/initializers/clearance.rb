888e0498ad63dd386849d7190345b5e714e940ad
lislis <mail@lislis.de>
2019-08-27 22:41:21 +0200

    attempt to fix clearance

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -3,4 +3,5 @@ Clearance.configure do |config|
   config.mailer_sender = "info@slamalphas.org"
   config.cookie_domain = ".slamalphas.org"
   config.secure_cookie = true
+  config.rotate_csrf_on_sign_in = true
 end
