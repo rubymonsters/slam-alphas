6366bf93415f014801c85aba7bfa32d320eff5a9
lislis <mail@lislis.de>
2019-09-16 20:23:41 +0200

    just dont cache at all

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,12 @@
 require 'redis'
 
 if Rails.env.production?
-  REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+  #REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
 
   Geocoder.configure(
     timeout: 3,
     lookup: :mapbox,
-    api_key: Rails.application.secrets.mapbox_api_key,
-    cache: Redis.new
+    api_key: Rails.application.secrets.mapbox_api_key
+    #cache: Redis.new
   )
 end
