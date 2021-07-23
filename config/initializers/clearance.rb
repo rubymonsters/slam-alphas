1398034f9990a672072b4a2a6e5c8335039240cc
lislis <mail@lislis.de>
2021-07-23 18:09:48 +0200

    bump clearance, set cookie seetings for prod

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,7 +1,11 @@
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "info@slamalphas.org"
-  config.cookie_domain = ".slamalphas.org"
-  config.secure_cookie = true
+  config.cookie_domain = ".slamalphas.org" if Rails.env.production?
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  config.cookie_name = "slam_alphas_remember_token"
+  config.secure_cookie = true if Rails.env.production?
+  config.routes = false
+  config.redirect_url = "/"
   config.rotate_csrf_on_sign_in = true
 end
