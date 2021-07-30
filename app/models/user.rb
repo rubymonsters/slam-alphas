6821f6164c687646a7c0376beb183fb3baf7319d
lislis <lislis@users.noreply.github.com>
2021-07-30 16:03:10 +0200

    add pronoun field to user (#279)

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,16 +56,14 @@ class User < ActiveRecord::Base
 
   has_many :events
 
-  validates :name, :city, :email, :country, :year_of_birth, presence: true
+  validates :name, :city, :email, :country, :year_of_birth, :pronouns, presence: true
   validates :name, length: { minimum: 2 }
   validates :email, uniqueness: { case_sensitive: false }, on: [:create, :update]
   validates :country, inclusion:  COUNTRIES.keys.map { |k| k.to_s }
   validates :password, confirmation: true, unless: :persisted?
 
-  # customized validity check in app/validators/email_validator.rb
   validates :email, email: true
 
-  # customized validity check in app/validators/website_validator.rb
   validates :website, website: true, allow_blank: true
   validates :facebook_link, website: true, allow_blank: true
   validates :video_link, website: true, allow_blank: true
