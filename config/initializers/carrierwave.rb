9ae7cf679495a5198d670e8c791b4652fbad5db8
lislis <lislis@users.noreply.github.com>
2022-01-05 18:34:59 +0100

    bring back carrierwave config (#295)
    * bring back carrierwave config
    
    * go back to fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,31 @@
-# Define an application-wide HTTP permissions policy. For further
-# information see https://developers.google.com/web/updates/2018/06/feature-policy
-#
-# Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
-# end
+require 'carrierwave/orm/activerecord'
+require 'carrierwave/processing/mini_magick'
+require 'fog/aws'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['S3_REGION'],
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public     = false
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{90.day.to_i}" }
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
+
+  # file access permissions
+  # config.permissions = 0666
+  # config.directory_permissions = 0777
+
+  # config.asset_host = "CDN LINK HERE"
+
+  # Error messages
+  config.ignore_integrity_errors = false
+  config.ignore_processing_errors = false
+  config.ignore_download_errors = false
+end
